212d6e554c78c8a729cdd60b5d7f0a6f148d6765
Jonathan <jonathankyule2@gmail.com>
2023-09-22 06:20:01 -0700

    task 1

--- sort.h
+++ sort.h
@@ -33,3 +33,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
+void insertion_sort_list(listint_t **list);
+void custom_swap_nodes(listint_t **head, listint_t **node1, listint_t *node2);
+
